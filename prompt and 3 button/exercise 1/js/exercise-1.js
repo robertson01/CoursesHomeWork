9ad9fcf192a64a91d668e692df6da722e8b2fdf1
robertson <kolyn9gg@gmail.com>
2018-10-24 12:39:34 +0300

    Add change for exersice

--- prompt and 3 button/exercise 1/js/exercise-1.js
+++ prompt and 3 button/exercise 1/js/exercise-1.js
@@ -4,6 +4,14 @@ let resulting = "";
 
 
 if (enterNumber.match(/\d/g)) {
+
+  // не совсем понял задание, сделал 2 варината. тут выдоит если есть цифра в имени, все буквы в Верхнем регистреж
+// for (let j = 0; j < enterNumber.length; j++) {
+//       resulting += enterNumber.charAt(j).toUpperCase();
+//   }
+//   alert(resulting);
+
+// если есть цифра в нике, то меняет места регист букв с большого на маленький и на оборот.
   for (let j = 0; j < enterNumber.length; j++) {
     if(enterNumber.charAt(j) == enterNumber.charAt(j).toLowerCase()){
       resulting += enterNumber.charAt(j).toUpperCase();
@@ -15,7 +23,7 @@ if (enterNumber.match(/\d/g)) {
   alert(resulting);
 }
 else{
-  // alert(enterNumber.split("").reverse().join(""));
+  // alert(enterNumber.split("").reverse().join("")); // один из варинтов решения.
   for (let i = enterNumber.length -1; i >= 0; i--) {
     result += enterNumber.charAt(i);
   }
